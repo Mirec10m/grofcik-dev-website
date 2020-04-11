83268fcce7f53e633be676672c66834be4f0c49b
Miroslav Grofčík <miroslav.grofcik@demi.sk>
2020-04-11 22:22:11 +0200

    Add SuperAdminMiddleware (objects-3)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'super_admin' => \App\Http\Middleware\SuperAdminMiddleware::class,
     ];
 }
