38376a89a5b715eca4f752c153241f6b59f37d6c
Miroslav Grofčík <miroslav.grofcik@demi.sk>
2020-04-15 16:16:09 +0200

    Change admin index text (admin-7)

--- resources/views/admin/_partials/_footer.blade.php
+++ resources/views/admin/_partials/_footer.blade.php
@@ -2,7 +2,7 @@
     © {{ \Carbon\Carbon::now()->year }} Demi Box -
     <span class="d-none d-sm-inline-block">
         Vytvorila spoločnosť
-        <a href="https://www.demi.sk/" target="_blank">
+        <a href="https://www.demi.sk/" target="_blank" class="color-demi link-demi">
             <b>DeMi Studio</b>
         </a>
     </span>.
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -7,7 +7,7 @@
             <div class="row">
                 <div class="col-sm-12">
                     <div class="page-title-box">
-                        <h4 class="page-title">Demi Box v 3.0</h4>
+                        <h4 class="page-title color-primary">Demi Box v 3.0</h4>
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item active">
                                 Vitajte vo Vašej administrácii
@@ -21,9 +21,34 @@
             <div class="row">
                 <div class="col-sm-12">
                     <div class="page-title-box">
-                        <h4 class="page-title">Sprievodný text</h4>
+                        <h4 class="page-title">Úvod</h4>
                         <div>
-                            blablabla
+                            Tento administračný systém bol vytvorený v DeMi Studio, s. r. o.
+                            <br>
+                            <br>
+                            Zostaňte s nami v kontakte:
+                            <ul class="admin-list">
+                                <li class="relative">
+                                    <a href="https://www.demi.sk/" target="_blank" class="admin-list-logo">
+                                        <img src="{{ asset('img/znak.svg') }}" class="img-responsive" alt="Demi Studio logo" />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/Demi-Studio-114844622479287/" target="_blank">
+                                        <i class="fab fa-facebook fa-2x"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.instagram.com/demi__studio/" target="_blank">
+                                        <i class="fab fa-instagram fa-2x"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.linkedin.com/company/demi-studio/" target="_blank">
+                                        <i class="fab fa-linkedin fa-2x"></i>
+                                    </a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
@@ -34,8 +59,7 @@
                     <div class="page-title-box">
                         <h4 class="page-title">Potrebujete pomoc?</h4>
                         <div>
-                            V prípade potreby nás prosím kontaktujte e-mailom, v nevyhnutno prípade
-                            použite telefónne číslo.
+                            V prípade potreby nás prosím kontaktujte e-mailom.
                             <br><br>
                             E-mail: <b>support@demi.sk</b>
                             <br>
