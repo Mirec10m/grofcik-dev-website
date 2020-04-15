44d109a3c518442ae162122c80aefb3bab6309bd
Miroslav Grofčík <miroslav.grofcik@demi.sk>
2020-04-15 19:07:04 +0200

    Fix routes (develop)

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-
 foreach(config('settings.languages') as $lang => $name){
     $prefix = $lang === config('app.locale') ? '' : $lang;
 
