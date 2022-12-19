231aec6f7e7dc5a8f3471c2e58c78bc99d226694
Lukáš Grofčík <lukas.grofcik@demi.sk>
2022-12-19 11:26:35 +0100

    Add dashboard (template)

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,7 +1,79 @@
 @extends('layout.admin')
 
 @section('content')
-    <div class="content">
+    <div class="page-content">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-sm-12">
+                    <h4 class="fs-16 mb-1">Demi Box v 3.0</h4>
+
+                    <p class="text-muted mb-0">Vitajte vo Vašej administrácii</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-12">
+                    <h4 class="fs-16 mb-1">Úvod</h4>
+
+                    <div>
+                        Tento administračný systém bol vytvorený v DeMi Studio, s. r. o.
+                        <br>
+                        <br>
+
+                        <p class="sub-title">Starostlivosť o Váš web</p>
+                        <div>
+                            Tu si môžete pozrieť ponuku balíkov starostlivosti o Váš web. Každý z našich balíkov starostlivosti okrem iného zahrňuje aj monitorovanie Vášho webu <strong>24/7</strong> pomocou externého softvéru.
+                        </div>
+                        <br>
+                        <p class="sub-title">Príklad z praxe</p>
+                        <div class="practice-text">
+                            <i>Používateľ / Zákazník príde na web / eshop a pri interakcii s aplikáciou mu vyhodí chybu. Pomocou monitorovacieho softvéru je chyba automaticky odchytená a odoslaná na náš email. K tomuto emailu majú prístup naši programátori, ktorí problém automaticky odstránia aby sa už neopakoval ďalšiemu návštevníkovi Vašich webových stránok. — V prípade, že web nie je monitorovaný, vlastník webu sa o probléme nemusí dozvedieť, až kým mu ho nenahlási niektorý z návštevníkov webových stránok.</i>
+                        </div>
+                        <ul class="admin-list offers">
+                        </ul>
+
+                        Zostaňte s nami v kontakte:
+                        <ul class="admin-list">
+                            <li class="relative">
+                                <a href="https://www.demi.sk/" target="_blank" class="admin-list-logo">
+                                    <img src="{{ asset('img/znak.svg') }}" class="img-responsive" alt="Demi Studio logo" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com/Demi-Studio-114844622479287/" target="_blank">
+                                    <i class="fab fa-facebook fa-2x"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.instagram.com/demi__studio/" target="_blank">
+                                    <i class="fab fa-instagram fa-2x"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/company/demi-studio/" target="_blank">
+                                    <i class="fab fa-linkedin fa-2x"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <h4 class="fs-16 mb-1">Potrebujete pomoc?</h4>
+
+            <div>
+                V prípade potreby nás prosím kontaktujte e-mailom.
+                <br><br>
+                E-mail: <b>support@demi.sk</b>
+                <br>
+                Tel. č.: <b>+421 918 735 863</b>
+            </div>
+
+        </div>
+    </div>
+
+    <div style="display:none;" class="page-content">
         <div class="container-fluid">
 
             <div class="row">
