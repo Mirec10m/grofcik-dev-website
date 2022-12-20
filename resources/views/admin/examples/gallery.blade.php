fef4c52e31dccd9bfe9392ef8d7bf3b43e4dab1c
Lukáš Grofčík <lukas.grofcik@demi.sk>
2022-12-20 10:20:57 +0100

    Add galery (template)

--- resources/views/admin/examples/gallery.blade.php
+++ resources/views/admin/examples/gallery.blade.php
@@ -5,33 +5,23 @@
 @endsection
 
 @section('content')
-    <div class="content">
+    <div class="page-content">
         <div class="container-fluid">
+            @include('admin._partials._breadcrumbs', [ 'title' => 'Examples', 'crumbs' => [ 'Demibox' ]])
 
             <div class="row">
-                <div class="col-sm-12">
-                    <div class="page-title-box">
-                        <h4 class="page-title">Examples</h4>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-12">
-                    <div class="card m-b-20">
+                <div class="col-lg-12">
+                    <div class="card">
                         <div class="card-body">
-
-                            <div class="row">
-                                <div class="col-12 col-sm-6">
-                                    <h4 class="mt-0 header-title">Galéria položky - {{-- $item->name_sk --}}Lorem ipsum</h4>
+                            <div class="row mb-4">
+                                <div class="col-sm-6">
+                                    <h5 class="card-title mb-0">Galéria položky - {{-- $item->name_sk --}}Lorem ipsum</h5>
                                 </div>
-                                <div class="col-12 col-sm-6">
-                                    <p class="text-muted m-b-30 text-right">
-                                        <a href="{{ route('examples.index') }}" class="btn btn-primary waves-effect waves-light">
-                                            <i class="fa fa-list pr-2"></i>
-                                            Zoznam položiek
-                                        </a>
-                                    </p>
+
+                                <div class="col-sm-6 text-right">
+                                    <a href="{{ route('examples.index') }}" type="button" class="btn btn-primary waves-effect waves-light float-end">
+                                        <i class="fa fa-list pr-2"></i> Zoznam položiek
+                                    </a>
                                 </div>
                             </div>
 
@@ -39,15 +29,14 @@
 
                             <form action="javascript:void(0)" method="post">
                                 @csrf
-                                <div class="row mb-3">
-                                    <div class="col-sm-12 col-md-9">
 
-                                        <div class="col-sm-6">
-                                            <div class="form-group">
-                                                <label>Obrázok</label>
-                                                <input name="image" type="file" value="{{ old('image', isset($item) ? $item->image : '') }}" class="form-control filestyle {{ $errors->has('image') ? 'parsley-error' : '' }}" data-buttonname="btn-secondary">
-                                                @include('admin._partials._errors', ['column' => 'image'])
-                                            </div>
+                                <div class="row mb-3">
+                                    <div class="col-sm-4">
+
+                                        <div>
+                                            <label for="image" class="form-label">Obrázok</label>
+                                            <input name="image" class="form-control {{ $errors->has('image') ? 'is-invalid' : '' }}" type="file" id="image">
+                                            @include('admin._partials._errors', ['column' => 'image'])
                                         </div>
 
                                     </div>
@@ -58,44 +47,41 @@
 
                             <div class="dropdown-divider"></div>
 
-                            <div class="row mt-3">
+                            <div class="row">
                                 <div class="col-sm-12">
-                                    <h4 class="mt-0 header-title">Obrázok</h4>
+                                    <h5 class="card-title mb-0">Obrázok</h5>
 
-                                    @if(1)
-                                        <div class="">
-                                            <div class="row">
-                                                @for($i = 0; $i < 8; $i++)
-                                                <div class="col-sm-3">
-                                                    <div class="image-wrapper mb-5">
-                                                        <img src="{{ asset('img/image-placeholder.jpg') }}" class="img-responsive">
-                                                        <div class="image-wrapper-back">
-                                                            <div class="image-wrapper-back-actions">
-                                                                <a href="{{ asset('img/image-placeholder.jpg') }}" class="show-icon image-popup-vertical-fit">
-                                                                    <i class="far fa-eye"></i>
-                                                                </a>
-                                                                <form action="javascript:void(0)" method="post">
-                                                                    @csrf
-                                                                    <button data-entity="{{ 'Obrázok - ' . 'image-placeholder.jpg' }}" class="delete-button delete-icon pointer" type="button">
-                                                                        <i class="fas fa-trash-alt"></i>
-                                                                    </button>
-                                                                </form>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                @endfor
-                                            </div>
-                                        </div>
-                                    @endif
                                 </div>
                             </div>
 
+                            <div class="row">
+                                @foreach(range(1, 8) as $i)
+                                    <div class="col-sm-3">
+                                        <div class="image-wrapper mb-3">
+                                            <img src="{{ asset('img/image-placeholder.jpg') }}" class="img-fluid">
+
+                                            <div class="image-wrapper-back">
+                                                <div class="image-wrapper-back-actions">
+                                                    <a href="{{ asset('img/image-placeholder.jpg') }}" class="show-icon image-popup-vertical-fit">
+                                                        <i class="mdi mdi-eye-outline"></i>
+                                                    </a>
+
+                                                    <form action="javascript:void(0)" method="post">
+                                                        @csrf
+                                                        <button data-entity="{{ 'Obrázok - ' . 'image-placeholder.jpg' }}" class="delete-button delete-icon pointer" type="button">
+                                                            <i class="mdi mdi-trash-can-outline"></i>
+                                                        </button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 @endsection
