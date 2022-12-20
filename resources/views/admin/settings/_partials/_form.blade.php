9aa9dc172f1dc4071a8b505007ed3e716fa4266a
Lukáš Grofčík <lukas.grofcik@demi.sk>
2022-12-20 10:50:21 +0100

    Add settings, password (template)

--- resources/views/admin/settings/_partials/_form.blade.php
+++ resources/views/admin/settings/_partials/_form.blade.php
@@ -1,37 +1,29 @@
 <div class="row mb-3">
     <div class="col-sm-12 col-md-9">
 
-        <div class="row">
+        <div class="row mb-3">
             <div class="col-sm-6">
-                <div class="form-group">
-                    <label>E-mail</label>
-                    <input type="text" disabled value="{{ $user->email }}" class="form-control">
-                </div>
+                <label class="form-label">E-mail</label>
+                <input type="text" disabled value="{{ $user->email }}" class="form-control">
             </div>
 
             <div class="col-sm-6">
-                <div class="form-group">
-                    <label>Používateľské meno</label>
-                    <input type="text" disabled value="{{ $user->username }}" class="form-control">
-                </div>
+                <label class="form-label">Používateľské meno</label>
+                <input type="text" disabled value="{{ $user->username }}" class="form-control">
             </div>
         </div>
 
-        <div class="row">
+        <div class="row mb-3">
             <div class="col-sm-6">
-                <div class="form-group">
-                    <label>Meno</label>
-                    <input name="name" type="text" value="{{ old('name', isset($user) ? $user->name : '') }}" class="form-control {{ $errors->has('name') ? 'parsley-error' : '' }}">
-                    @include('admin._partials._errors', ['column' => 'name'])
-                </div>
+                <label class="form-label">Meno</label>
+                <input name="name" type="text" value="{{ old("name", isset($user) ? $user->name : '') }}" class="form-control {{ $errors->has("name") ? 'is-invalid' : '' }}">
+                @include('admin._partials._errors', ['column' => "name"])
             </div>
 
             <div class="col-sm-6">
-                <div class="form-group">
-                    <label>Priezvisko</label>
-                    <input name="surname" type="text" value="{{ old('surname', isset($user) ? $user->surname : '') }}" class="form-control {{ $errors->has('surname') ? 'parsley-error' : '' }}">
-                    @include('admin._partials._errors', ['column' => 'surname'])
-                </div>
+                <label class="form-label">Priezvisko</label>
+                <input name="surname" type="text" value="{{ old("surname", isset($user) ? $user->surname : '') }}" class="form-control {{ $errors->has("surname") ? 'is-invalid' : '' }}">
+                @include('admin._partials._errors', ['column' => "surname"])
             </div>
         </div>
 
