e226e1792e714189462e8d70399fe45db25a28fe
Lukas Grofcik <lukas.grofcik@demi.sk>
2023-04-03 14:16:48 +0200

    Fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -60,8 +60,10 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::middleware('web')
             ->namespace($this->namespace)
-            ->group(base_path('routes/web.php'))
-            ->group(base_path('routes/demibox.php'));
+            ->group([
+                base_path('routes/web.php'),
+                base_path('routes/demibox.php'),
+            ]);
     }
 
     /**
