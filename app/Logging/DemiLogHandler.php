33ffb9e949e895b375a65cac29a738fdf2babe9c
Lukas Grofcik <lukas.grofcik@demi.sk>
2023-04-03 14:00:55 +0200

    Fix (fix)

--- app/Logging/DemiLogHandler.php
+++ app/Logging/DemiLogHandler.php
@@ -2,9 +2,6 @@
 
 namespace App\Logging;
 
-use Monolog\Level;
-use Monolog\Logger;
-use Monolog\LogRecord;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mailer\Transport;
@@ -15,21 +12,42 @@ class DemiLogHandler extends SymfonyMailerHandler
 
     public function __construct($from, $to = 'support@demi.sk')
     {
-        $username = urlencode( config('mail.mailers.smtp.username') );
-        $password = urlencode( config('mail.mailers.smtp.password') );
+        $username = config('mail.mailers.smtp.username');
+        $password = config('mail.mailers.smtp.password');
         $host = config('mail.mailers.smtp.host');
         $port = config('mail.mailers.smtp.port');
 
+        if ( ! isset($username, $password, $host, $port) || ! env('DEMI_LOG') ) {
+            return;
+        }
+
+        $mailer = $this->getMailer(urlencode($username), urlencode($password), $host, $port);
+
+        $email = $this->getErrorMail($from, $to);
+
+        parent::__construct($mailer, $email);
+    }
+
+    private function getMailer(string $username, string $password, string $host, string $port) : Mailer
+    {
         $transport = Transport::fromDsn("smtp://$username:$password@$host:$port");
 
-        $mailer = new Mailer($transport);
+        return new Mailer($transport);
+    }
 
-        $email = (new Email())
+    private function getErrorMail(string $from, string $to) : Email
+    {
+        return (new Email())
             ->subject("Demi Log Error")
             ->from($from)
             ->to($to);
+    }
 
-        parent::__construct($mailer, $email);
+    protected function send(string $content, array $records): void
+    {
+        if( ! env('DEMI_LOG') ) return;
+
+        parent::send($content, $records);
     }
 
 }
