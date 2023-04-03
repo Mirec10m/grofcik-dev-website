caea605e4fd0806d417e3fbdccf8ee79bd3ad40c
Lukas Grofcik <lukas.grofcik@demi.sk>
2023-04-03 16:37:27 +0200

    Fix post form tabs, merge post form general and other (tweaks)

--- resources/views/admin/posts/_partials/_form.blade.php
+++ resources/views/admin/posts/_partials/_form.blade.php
@@ -26,6 +26,54 @@
 
                                 <div class="col-sm-6">
                                     <label class="form-label">
+                                        Slug <span class="text-uppercase">{{ $key }}</span> <span class="text-danger">*</span>
+                                    </label>
+                                    <input name="slug_{{ $key }}" type="text" value="{{ old("slug_$key", isset($post) ? $post->{"slug_$key"} : '') }}" class="form-control {{ $errors->has("slug_$key") ? 'is-invalid' : '' }}">
+                                    @include('admin._partials._errors', ['column' => "slug_$key"])
+                                </div>
+                            </div>
+
+                            @if( config('demibox.blog.categories') || config('demibox.blog.tags') )
+                                <div class="row mb-3">
+                                    @if( config('demibox.blog.categories') )
+                                        <div class="col-sm-6">
+                                            <label class="form-label">
+                                                Kategória <span class="text-danger">*</span>
+                                            </label>
+                                            <select name="post_category_id" class="form-control js-example-basic-single {{ $errors->has('post_category_id') ? 'is-invalid' : '' }}">
+                                                <option value="">Vyberte kategóriu</option>
+                                                @foreach($post_categories as $post_category)
+                                                    <option value="{{ $post_category->id }}" {{ old('post_category_id') == $post_category->id ? 'selected' : (isset($post) && $post->post_category_id == $post_category->id ? 'selected' : '') }}>
+                                                        {{ $post_category->name_sk }}
+                                                    </option>
+                                                @endforeach
+                                            </select>
+                                            @include('admin._partials._errors', ['column' => 'post_category_id'])
+                                        </div>
+                                    @endif
+
+                                    @if( config('demibox.blog.tags') )
+                                        <div class="col-sm-6">
+                                            <label class="form-label">
+                                                Tagy <span class="text-danger">*</span>
+                                            </label>
+                                            <select name="tags[]" class="form-control js-example-basic-multiple {{ $errors->has('tags') ? 'is-invalid' : '' }}" multiple>
+                                                @foreach($post_tags as $post_tag)
+                                                    <option value="{{ $post_tag->id }}" {{ in_array($post_tag->id, old('tags', [])) ? 'selected' : (isset($post) && $post->tags->contains('id', $post_tag->id) ? 'selected' : '') }}>
+                                                        {{ $post_tag->name_sk }}
+                                                    </option>
+                                                @endforeach
+                                            </select>
+                                            @include('admin._partials._errors', ['column' => 'tags'])
+                                            @include('admin._partials._errors', ['column' => 'tags.*'])
+                                        </div>
+                                    @endif
+                                </div>
+                            @endif
+
+                            <div class="row mb-3">
+                                <div class="col-sm-6">
+                                    <label class="form-label">
                                         Zobraziť <span class="text-uppercase">{{ $key }}</span>
                                     </label>
                                     <div class="form-check form-switch form-switch-info form-switch-lg" dir="ltr">
@@ -33,16 +81,6 @@
                                         <input name="published_{{ $key }}" type="checkbox" class="form-check-input {{ $errors->has("published_$key") ? 'is-invalid' : '' }}" value="1" {{ old("published_$key") == 1 ? 'checked' : (isset($post) && $post->{"published_$key"} ? 'checked' : '') }}>
                                     </div>
                                     @include('admin._partials._errors', ['column' => "published_$key"])
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <div class="col-sm-6">
-                                    <label class="form-label">
-                                        Slug <span class="text-uppercase">{{ $key }}</span> <span class="text-danger">*</span>
-                                    </label>
-                                    <input name="slug_{{ $key }}" type="text" value="{{ old("slug_$key", isset($post) ? $post->{"slug_$key"} : '') }}" class="form-control {{ $errors->has("slug_$key") ? 'is-invalid' : '' }}">
-                                    @include('admin._partials._errors', ['column' => "slug_$key"])
                                 </div>
                             </div>
 
@@ -195,55 +233,68 @@
         </div>
     </div>
 
-    @if( config('demibox.blog.categories') || config('demibox.blog.tags') )
-        <div class="tab-pane p-3" id="other" role="tabpanel">
-            <div class="row mb-4">
-                <div class="col-sm-6">
-                    <h5 class="card-title mb-0">{{ $card_title }} - Ostatné</h5>
-                </div>
-
-                @include('admin.posts._partials._form_buttons')
+    <div class="tab-pane p-3" id="seo" data-show-image="true" role="tabpanel">
+        <div class="row mb-4">
+            <div class="col-sm-6">
+                <h5 class="card-title mb-0">{{ $card_title }} - SEO</h5>
             </div>
 
-            <div class="row mb-3">
-                <div class="col-sm-12 col-md-9">
-                    <div class="row mb-3">
-                        @if( config('demibox.blog.categories') )
-                            <div class="col-sm-6">
-                                <label class="form-label">
-                                    Kategória <span class="text-danger">*</span>
-                                </label>
-                                <select name="post_category_id" class="form-control js-example-basic-single {{ $errors->has('post_category_id') ? 'is-invalid' : '' }}">
-                                    <option value="">Vyberte kategóriu</option>
-                                    @foreach($post_categories as $post_category)
-                                        <option value="{{ $post_category->id }}" {{ old('post_category_id') == $post_category->id ? 'selected' : (isset($post) && $post->post_category_id == $post_category->id ? 'selected' : '') }}>
-                                            {{ $post_category->name_sk }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                @include('admin._partials._errors', ['column' => 'post_category_id'])
-                            </div>
-                        @endif
+            @include('admin.posts._partials._form_buttons')
+        </div>
 
-                        @if( config('demibox.blog.tags') )
-                            <div class="col-sm-6">
-                                <label class="form-label">
-                                    Tagy <span class="text-danger">*</span>
-                                </label>
-                                <select name="tags[]" class="form-control js-example-basic-multiple {{ $errors->has('tags') ? 'is-invalid' : '' }}" multiple>
-                                    @foreach($post_tags as $post_tag)
-                                        <option value="{{ $post_tag->id }}" {{ in_array($post_tag->id, old('tags', [])) ? 'selected' : (isset($post) && $post->tags->contains('id', $post_tag->id) ? 'selected' : '') }}>
-                                            {{ $post_tag->name_sk }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                @include('admin._partials._errors', ['column' => 'tags'])
-                                @include('admin._partials._errors', ['column' => 'tags.*'])
+        @include('admin._partials._lang_tabs', [ 'inputs' => ['name', 'published', 'slug', 'short'], 'key_append' => '_profile' ])
+
+        <div class="tab-content mb-4">
+            @foreach( config('settings.languages') as $key => $lang )
+                <div class="tab-pane p-3 {{ $loop->first ? 'active' : '' }}" id="{{ $key }}_profile" role="tabpanel">
+                    <div class="row mb-3">
+                        <div class="col-sm-6">
+                            <label class="form-label">
+                                Názov <span class="text-uppercase">{{ $key }}</span> <span class="text-danger">*</span>
+                            </label>
+                            <div class="input-group">
+                                <input name="profile_name_{{ $key }}" type="text" value="{{ old("profile_name_$key", isset($post) ? $post->{"profile_name_$key"} : '') }}" class="form-control {{ $errors->has("profile_name_$key") ? 'is-invalid' : '' }}">
+                                <span class="input-group-text" id="basic-addon1">.webp</span>
                             </div>
-                        @endif
+                            @include('admin._partials._errors', ['column' => "profile_name_$key"])
+                        </div>
+
+                        <div class="col-sm-6">
+                            <label class="form-label">
+                                Alternatívny text <span class="text-uppercase">{{ $key }}</span> <span class="text-danger">*</span>
+                            </label>
+                            <input name="profile_alt_{{ $key }}" type="text" value="{{ old("profile_alt_$key", isset($post) ? $post->{"profile_alt_$key"} : '') }}" class="form-control {{ $errors->has("profile_alt_$key") ? 'is-invalid' : '' }}">
+                            @include('admin._partials._errors', ['column' => "profile_alt_$key"])
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col-sm-12">
+                            <label class="form-label">
+                                Krátky popis <span class="text-uppercase">{{ $key }}</span> (max. 255 znakov) <span class="text-danger">*</span>
+                            </label>
+                            <textarea name="profile_description_{{ $key }}" class="form-control {{ $errors->has("profile_description_$key") ? 'is-invalid' : '' }}">{{ old("profile_description_$key", isset($post) ? $post->{"profile_description_$key"} : '') }}</textarea>
+                            @include('admin._partials._errors', ['column' => "profile_description_$key"])
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+        <div class="row mb-3">
+            <div class="col-sm-12 col-md-9">
+                <div class="row mb-3">
+                    <div class="col-sm-6">
+                        <div>
+                            <label class="form-label">
+                                Profilový obrázok <span class="text-danger">*</span>
+                            </label>
+                            <input name="profile" class="form-control filestyle {{ $errors->has('profile') ? 'is-invalid' : '' }}" type="file" data-text="Vybrať súbor" data-btnClass="btn-primary border-left-no-radius">
+                            @include('admin._partials._errors', ['column' => 'profile'])
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    @endif
+    </div>
 </div>
--- resources/views/admin/posts/_partials/_tabs.blade.php
+++ resources/views/admin/posts/_partials/_tabs.blade.php
@@ -1,6 +1,6 @@
 <ul class="nav nav-tabs custom-tabs" role="tablist">
     <li class="nav-item">
-        <a class="nav-link active {{ has_language_errors($errors, ['name', 'published', 'slug', 'short']) ? 'is-invalid' : '' }}" data-bs-toggle="tab" href="#info" role="tab" aria-selected="false">
+        <a class="nav-link active {{ has_language_errors($errors, ['name', 'published', 'slug', 'short']) || has_errors($errors, ['post_category_id', 'tags', 'tags.*']) ? 'is-invalid' : '' }}" data-bs-toggle="tab" href="#info" role="tab" aria-selected="false">
             <i class="mdi mdi-pencil mr-6"></i>
             Všeobecné
         </a>
@@ -20,12 +20,10 @@
         </a>
     </li>
 
-    @if( config('demibox.blog.categories') || config('demibox.blog.tags') )
-        <li class="nav-item">
-            <a class="nav-link {{ has_errors($errors, ['category_id', 'tags', 'tags.*']) ? 'is-invalid' : '' }}" data-bs-toggle="tab" href="#other" role="tab" aria-selected="false">
-                <i class="mdi mdi-format-list-bulleted mr-6"></i>
-                Ostatné
-            </a>
-        </li>
-    @endif
+    <li class="nav-item">
+        <a class="nav-link {{ has_errors($errors, []) ? 'is-invalid' : '' }}" data-bs-toggle="tab" href="#seo" role="tab" aria-selected="false">
+            <i class="mdi mdi-cog mr-6"></i>
+            SEO
+        </a>
+    </li>
 </ul>
